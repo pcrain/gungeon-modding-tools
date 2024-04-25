64c4de06824230f38a1f32cb024d655d46a67909
Patrick Crain <patrickacrain@gmail.com>
2024-04-24 22:05:22 -0400

    Fix small issue with path resolution

--- gungeon-gun-sprite-json-creator.py
+++ gungeon-gun-sprite-json-creator.py
@@ -25,7 +25,7 @@ def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = os.path.dirname(os.path.realpath(__file__))
 
     return os.path.join(base_path, relative_path)
 
